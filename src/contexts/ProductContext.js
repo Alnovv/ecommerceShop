8ca62c1de7334c10e63fcf425ac816d6d2b3dee1
Alnovv <acadangan658@gmail.com>
2023-11-13 19:56:47 +0700

    first

---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,23 @@
+import React ,{createContext, useState, useEffect } from 'react';
+
+export const ProductContext = createContext ();
+
+
+const ProductProvider = ({children}) => {
+  const [products, setProducts] = useState([]);
+  
+  useEffect(()=>{
+    const fetchProductStore = async ()=> {
+      const response =await fetch('https://fakestoreapi.com/products')
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchProductStore();
+  },[])
+
+  return <ProductContext.Provider value={{products}} >
+    {children}
+  </ProductContext.Provider>;
+};
+
+export default ProductProvider;
